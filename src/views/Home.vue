2a1eb116e2572d601360c2e4b1b2d348d307c793
YuzeTT <ziv1024@163.com>
2021-08-09 13:01:34 +0800

    作业列表升级vue3-reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -50,6 +50,7 @@
             </el-col>
           </el-row>
           <el-collapse v-model="activeNames" v-for="(item,index) in homeWork" :key="item">
+            {{activeNames}}
             <el-collapse-item :title=item.title :name=index>
               <!-- <div>{{ item.list }}</div> -->
               <!-- <ul>
@@ -80,31 +81,6 @@
                 
               </el-table>
             </el-collapse-item>
-            <!-- <el-collapse-item title="数学" name="2">
-              <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-              <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-            </el-collapse-item>
-            <el-collapse-item title="英语" name="3">
-              <div>简化流程：设计简洁直观的操作流程；</div>
-              <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-              <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
-            </el-collapse-item>
-            <el-collapse-item title="历史" name="4">
-              <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-              <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
-            </el-collapse-item>
-            <el-collapse-item title="地理" name="5">
-              <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-              <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
-            </el-collapse-item>
-            <el-collapse-item title="政治" name="6">
-              <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-              <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
-            </el-collapse-item>
-            <el-collapse-item title="生物" name="7">
-              <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-              <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
-            </el-collapse-item> -->
           </el-collapse>
         </Card>
 
@@ -134,6 +110,7 @@
 
 <script>
 // @ is an alias to /src
+import {reactive,toRefs} from 'vue'
 import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
 import Hello from '@/components/Hello.vue'
@@ -145,6 +122,20 @@ import Error from '@/components/Error.vue'
 
 export default {
   name: 'Home',
+  setup() {
+    const homeWorkList = reactive({
+      activeNames: [],
+      showAll: () => {
+        homeWorkList.activeNames = ['1','2','3','4','5','6','7']
+      },
+      hideAll: () => {
+        homeWorkList.activeNames = []
+      }
+    })
+    return {
+      ...toRefs(homeWorkList)
+    }
+  },
   components: {
     Navbar,
     Footer,
@@ -155,7 +146,7 @@ export default {
   },
   data() {
     return {
-      activeNames: [],
+      
       userInfo: {
         column: 2,
       },
@@ -190,12 +181,6 @@ export default {
       })
   },
   methods: {
-    showAll:function() {
-      this.activeNames = ['1','2','3','4','5','6','7']
-    },
-    hideAll:function() {
-      this.activeNames = []
-    },
     getDate:function() {
       let d = new Date()
       let yy = d.getFullYear()
